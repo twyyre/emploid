55b1a4d678d9c6101d6e13b8bde5886bac4d225b
twyyre <108093992+twyyre@users.noreply.github.com>
2024-01-28 18:04:14 +0200

    cleaned files

--- other/mss_test01.py
+++
@@ -1,3 +0,0 @@
-from mss import mss
-with mss() as sct:
-    sct.shot()
--- other/start_chrome.py
+++
@@ -1,6 +0,0 @@
-from pywinauto import Desktop, Application
-
-chrome_dir = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
-page_url = 'http://10.10.20.44:4455/'
-chrome = Application().start(chrome_dir+' --force-renderer-accessibility --incognito --start-maximized ' + page_url)
-app_new_tab = Application(backend='uia').connect(path='chrome.exe', title_re='New Tab')
