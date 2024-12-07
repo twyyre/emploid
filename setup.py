e76532629fbb96badb7b7a551e236a6cf6cf5080
PixQuilly <pixquilly@gmail.com>
2024-12-07 09:27:02 +0200

    fixed dynamic requirement importing

--- setup.py
+++ setup.py
@@ -1,12 +1,28 @@
 from setuptools import setup
+from charset_normalizer import detect
+
+def process_requirements(_fname="requirements.txt"):
+   try:
+        with open(_fname, "rb") as f:
+            byte_content = f.read()
+            detected = detect(byte_content)
+            encoding = detected.get("encoding", "utf-8")  # Default to utf-8 if detection fails
+            content = byte_content.decode(encoding)
+            return [
+               line.strip() for line in content.splitlines()
+               if line.strip() and not line.startswith("#")
+            ]
+   except Exception as e:
+      print(f"Error reading requirements file: {e}")
+      return []
 
 setup(
    name='emploid',
-   version='0.4.1',
+   version='0.4.4',
    description='A simple to use automation tool for automating web, android and windows proccesses.',
    long_description='A simple to use automation tool for automating web, android and windows proccesses.',
    author='PixQuilly',
    author_email='pixquilly@gmail.com',
    packages=['emploid'],  #same as name
-   install_requires=['wheel', 'bar', 'greek', 'cv2', 'numpy', 'pyautogui'], #external packages as dependencies
+   install_requires=process_requirements(), #external packages as dependencies
 )
